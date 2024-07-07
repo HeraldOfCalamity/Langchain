45600461bc85e0441d7c0957a22f417a299eb088
Leonardo Eguino <releegva@gmail.com>
2024-07-07 15:34:23 -0400

    added(- five news to test), modified(main.py - implemented summarization by words length and paragraph count).

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from config import OPENAI_API_KEY
 
 llm = OpenAI(
     api_key=OPENAI_API_KEY,
+    max_tokens=-1,
 )
 
 summary_template_1 = PromptTemplate.from_template(
@@ -41,9 +42,24 @@ summary_template_5 = PromptTemplate.from_template(
                                                   Paragraph count: (Number of paragraphs does the text has)\n
                                                   Summary: (Summary of the text)."""
 )
+summary_template_6 = PromptTemplate.from_template(
+    """Process this text (text between: ```): ```{news_text}``` Then make a report guided by the following structure (where the summary is mandatory): 
+                                                  Title: (Title of the text or main topic)\n
+                                                  Word count: (Number of words does the text has)\n
+                                                  Paragraph count: (Number of paragraphs does the text has)\n
+                                                  Summary: (Text summary composed of exactly {summary_word_length} words )."""
+)
+summary_template_7 = PromptTemplate.from_template(
+    """Process this text (text between: ```): ```{news_text}``` Then make a report guided by the following structure (where the summary is mandatory): 
+                                                  Title: (Title of the text or main topic)\n
+                                                  Word count: (Number of words does the text has)\n
+                                                  Paragraph count: (Number of paragraphs does the text has)\n
+                                                  Summary: (Text summary composed of exactly {summary_paragraph_count} paragraphs )."""
+)
 
 
-news = get_new_at_index(4)
+
+news = get_new_at_index(9)
 
 
 prompt1 = summary_template_1.format(news_text=news)
@@ -52,8 +68,13 @@ prompt3 = summary_template_3.format(news_text=news)
 prompt4 = summary_template_4.format(news_text=news)
 prompt5 = summary_template_5.format(news_text=news)
 
+prompt6 = summary_template_6.format(news_text=news, summary_word_length=50)
+prompt7 = summary_template_7.format(news_text=news, summary_paragraph_count=2)
 
-response = llm.invoke(prompt5)
+
+
+
+response = llm.invoke(prompt7)
 
 print(response)
 print("========================================")
